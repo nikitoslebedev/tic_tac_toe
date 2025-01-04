7106f1dd8a13597ac52227810979730db152a80a
Nikita Lebedev <nikitka230104@mail.ru>
2025-01-04 17:36:48 +0300

    Крестики-нолики (визуальное)

--- gameparts/parts.py
+++ gameparts/parts.py
@@ -13,10 +13,10 @@ class Board:
     def make_move(self, row, col, player):
         self.board[row][col] = player
 
-    def display(self):
-        for row in self.board:
-            print('|'.join(row))
-            print('-' * 5)
+#    def display(self):
+#        for row in self.board:
+#            print('|'.join(row))
+#            print('-' * 5)
 
     def is_board_full(self):
         # Цикл проходится по всем столбцам игрового поля.
